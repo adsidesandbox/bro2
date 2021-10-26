bab3297fd19d62bc5db6af893abdfaa482b73a3c
adsidesandbox <93096842+adsidesandbox@users.noreply.github.com>
2021-10-27 01:26:01 +0300

    Add files via upload

---
+++ app/assets.js
@@ -0,0 +1,60 @@
+var enemySpriteTextureFiles=[];
+for(let i=0; i<24;i++){
+enemySpriteTextureFiles[i] = "assets/enemy/" + i + ".png";
+}
+
+
+enemySpriteTextureFiles[0] = "assets/enemy/0.png";
+enemySpriteTextureFiles[1] = "assets/enemy/1.png";
+enemySpriteTextureFiles[2] = "assets/enemy/2.png";
+enemySpriteTextureFiles[3] = "assets/enemy/3.png";
+enemySpriteTextureFiles[4] = "assets/enemy/4.png";
+enemySpriteTextureFiles[5] = "assets/enemy/5.png";
+enemySpriteTextureFiles[6] = "assets/enemy/6.png";
+enemySpriteTextureFiles[7] = "assets/enemy/7.png";
+enemySpriteTextureFiles[8] = "assets/enemy/8.png";
+enemySpriteTextureFiles[9] = "assets/enemy/9.png";
+enemySpriteTextureFiles[10] = "assets/enemy/10.png";
+enemySpriteTextureFiles[11] = "assets/enemy/11.png";
+
+
+var enemyBossTextureFile=[];
+for(let i=0; i<24;i++){
+enemyBossTextureFile[i] = "assets/enemyboss/" + i + "-min.png";
+}
+
+
+
+
+
+var player1TextureFile=[];
+player1TextureFile[0]="assets/player1/idle/00.png";
+
+var player2textureFile=[];
+for(let i=0; i<12;i++){
+player2textureFile[i] = "assets/player2/" + i + "-min.png";
+}
+
+
+var player3textureFile=[];
+player3textureFile[0]="assets/player3/shit.png";
+
+
+var player1AttackTextureFile=[];
+for(let i=0; i<12;i++){
+player1AttackTextureFile[i] = "assets/player1/attack" + i + ".png";
+}
+
+var flashTextureFile=[];
+for(let i=0; i<7;i++){
+flashTextureFile[i] = "assets/flash/" + i + ".png";
+}
+
+arrowSpriteTextureFile="assets/arrow.png";
+
+var icon1SpriteTextureFile="assets/ui_icon1.png";
+var icon2SpriteTextureFile="assets/ui_icon2.png";
+var icon3SpriteTextureFile="assets/ui_icon3.png";
+var uiCellFillSpriteTextureFile="assets/ui_cell_fill.png";
+var uiHandSpriteTextureFile="assets/ui_hand.png";
+var uiButtonSpriteTextureFile="assets/ui_button.png";
